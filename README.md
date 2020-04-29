d941d67625f703079e816144151c678de19a563c
Francesco Battaglia <fpbatta@Francescos-MacBook-Pro.local>
2020-04-29 12:12:49 +0200

    First round of linting and porting

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
+# Memory replay in balanced recurrent networks
+
+A port of the code from Chenkov et al. to current versions of Brian2 pipeline,
+as a stsarting point for current investigations
+
+Currently targeting:
+  - brian2=2.2.2.1
+  - numpy=1.18.1
+  - python=3.6.10
+
+
+
+### Old README
 This code reproduces some results from the paper:
 
 Memory replay in balanced recurrent networks.
 N. Chenkov, H. Sprekeler, R. Kempter
-PLoS Comput. Biol., 13(1):e1005359, 2017. DOI: 10.1371/journal.pcbi.1005359 
-http://journals.plos.org/ploscompbiol/article?id=10.1371/journal.pcbi.1005359
+[PLoS Comput. Biol., 13(1):e1005359, 2017. DOI: 10.1371/journal.pcbi.1005359](http://journals.plos.org/ploscompbiol/article?id=10.1371/journal.pcbi.1005359)
 
 The code managed to cold-rot, and old packages need to be imported.
 One way to run the program is to create a virtual environment.
--- calc_AC_FR.py
+++ calc_AC_FR.py
@@ -1,31 +1,30 @@
-from matplotlib import pyplot as plt 
+from matplotlib import pyplot as plt
 import numpy
 import calc_spikes
 
-
 dt = .1
-ww=2.
-t=numpy.arange(-3*ww,3*ww,dt)
-gas = numpy.exp(-(t/ww)**2)
-gas/=gas.sum()
+ww = 2.
+t = numpy.arange(-3 * ww, 3 * ww, dt)
+gas = numpy.exp(-(t / ww) ** 2)
+gas /= gas.sum()
 
-dur=40
+dur = 40
 tstart = 40
 
-def get_ac(nn,i=0):
-    x = calc_spikes.get_spike_train(nn.mon_spike_e[i],dur,dt,tstart)
-    y=numpy.correlate(x,gas,'same')  
-    z = numpy.correlate(y,y,'same')
+
+def get_ac(nn, i=0):
+    x = calc_spikes.get_spike_train(nn.mon_spike_e[i], dur, dt, tstart)
+    y = numpy.correlate(x, gas, 'same')
+    z = numpy.correlate(y, y, 'same')
 
     return z
 
 
-def get_mean_ac(nn,n=5):
+def get_mean_ac(nn, n=5):
     sh = 0
-    z=get_ac(nn,sh+0)
-    for i in range(n-1):
-        z+=get_ac(nn,sh+1+i)
+    z = get_ac(nn, sh + 0)
+    for i in range(n - 1):
+        z += get_ac(nn, sh + 1 + i)
 
     plt.plot(z)
     plt.show()
-
